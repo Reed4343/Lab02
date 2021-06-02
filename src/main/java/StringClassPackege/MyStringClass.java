a01ccfad97f2dbf239b1c42282b5bce48a0e4791
Professional <volk1119229@gmail.com>
2021-06-02 22:42:09 +0300

    a

---
+++ src/main/java/StringClassPackege/MyStringClass.java
@@ -0,0 +1,20 @@
+package StringClassPackege;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MyStringClass {
+    public static String stringToUpCase(String Line){
+        return Line.toUpperCase();
+    }
+    public static String stringToLowCase(String Line){
+
+        return Line.toLowerCase();
+    }
+    public static int stringLen(String Line){
+        return Line.length();
+    }
+    public static String stringUnion(String FirstLine, String SecondLine){
+        return FirstLine + SecondLine;
+    }
+}
